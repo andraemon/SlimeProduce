eb9e4daedde18d1cf90917b8ff11c2a04a717e28
andraemon <43394694+andraemon@users.noreply.github.com>
2022-12-06 13:57:49 -0500

    fixed spawn slime command, removed deprecated mod integration

--- SlimeProduce/ModEntry.cs
+++ SlimeProduce/ModEntry.cs
@@ -17,9 +17,9 @@ namespace SlimeProduce
     {
         public override void Entry(IModHelper helper)
         {
-            help = helper;
+            ModHelper = helper;
             SlimeHutchPatches.Initialize(Monitor);
-            Config = help.ReadConfig<ModConfig>();
+            Config = ModHelper.ReadConfig<ModConfig>();
 
             var harmony = new Harmony(ModManifest.UniqueID);
             harmony.Patch(
@@ -31,18 +31,21 @@ namespace SlimeProduce
                postfix: new HarmonyMethod(typeof(SlimeHutchPatches), nameof(SlimeHutchPatches.DayUpdate_Postfix))
             );
                         
-            help.Events.World.ObjectListChanged += World_ObjectListChanged;
-            help.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
-            help.ConsoleCommands.Add("spawn_slime", "Spawns slimes of a certain color.\n\nUsage: spawn_slime <r> <g> <b>\n- r/g/b: The values for the red, green or blue components of the slime's color. Should be integers between 0 and 255.", SpawnSlime);
+            ModHelper.Events.World.ObjectListChanged += World_ObjectListChanged;
+            ModHelper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
+
+            ModHelper.ConsoleCommands.Add("spawn_slime", "Spawns slimes of a certain color.\n\nUsage: spawn_slime <r> <g> <b>\n- r/g/b: The values for the red, green or blue components of the slime's color. Should be integers between 0 and 255.", SpawnSlime);
+
             Monitor.Log("Loaded", LogLevel.Debug);
         }
 
         private void GameLoop_GameLaunched(object sender, GameLaunchedEventArgs e)
         {
-            DeluxeGrabberLoaded = help.ModRegistry.IsLoaded("stokastic.DeluxeGrabber");
-            DeluxeGrabberReduxLoaded = help.ModRegistry.IsLoaded("ferdaber.DeluxeGrabberRedux");
-            if (DeluxeGrabberLoaded) Monitor.Log("Deluxe Auto-Grabber integration loaded", LogLevel.Debug);
-            else if (DeluxeGrabberReduxLoaded) Monitor.Log("Deluxe Auto-Grabber Redux integration loaded", LogLevel.Debug);
+            // Check if integrable mods are loaded (see if this can be done through new load order feature?)
+            DeluxeGrabberReduxLoaded = ModHelper.ModRegistry.IsLoaded("ferdaber.DeluxeGrabberRedux");
+            if (DeluxeGrabberReduxLoaded) Monitor.Log("Deluxe Auto-Grabber Redux integration loaded", LogLevel.Debug);
+
+            // Get dye objects for specil color drops
             foreach (KeyValuePair<int, string> pair in Game1.objectInformation)
             {
                 StardewValley.Object obj = new StardewValley.Object(pair.Key, 1, false, -1, 0);
@@ -79,18 +82,18 @@ namespace SlimeProduce
                 foreach (KeyValuePair<Vector2, StardewValley.Object> p in e.Removed)
                 {
                     ItemsToDrop.Clear();
-                    if (p.Value.Name.Contains("Slime Ball") && !string.IsNullOrEmpty(p.Value.orderData))
+                    if (p.Value.Name.Contains("Slime Ball") && !string.IsNullOrEmpty(p.Value.orderData.Value))
                     {
-                        Random r = new Random((int)(Game1.stats.DaysPlayed + Game1.uniqueIDForThisGame + p.Value.TileLocation.X * 77 + p.Value.TileLocation.Y * 777));
-                        if (Config.EnableSpecialColorDrops && ColoredObjects.ContainsKey(StrToColor(p.Value.orderData)))
+                        Random r = new((int)(Game1.stats.DaysPlayed + Game1.uniqueIDForThisGame + p.Value.TileLocation.X * 77 + p.Value.TileLocation.Y * 777));
+                        if (Config.EnableSpecialColorDrops && ColoredObjects.ContainsKey(StrToColor(p.Value.orderData.Value)))
                         {
                             if (r.NextDouble() < Config.SpecialColorDropChance)
                             {
-                                List<int> i = ColoredObjects[StrToColor(p.Value.orderData)];
+                                List<int> i = ColoredObjects[StrToColor(p.Value.orderData.Value)];
                                 ItemsToDrop.Add(i[r.Next(i.Count)], r.Next(Config.SpecialColorMinDrop, Config.SpecialColorMaxDrop + 1));
                             }
                         }
-                        if (Config.EnableSpecialTigerSlimeDrops && StrToColor(p.Value.orderData).Equals(new Color(255, 128, 0)) && int.Parse(p.Value.orderData.Value.Split('.')[4]) == 420)
+                        if (Config.EnableSpecialTigerSlimeDrops && StrToColor(p.Value.orderData.Value).Equals(new Color(255, 128, 0)) && int.Parse(p.Value.orderData.Value.Split('.')[4]) == 420)
                         {
                             ItemsToDrop.Add(92, r.Next(15, 26));
                             ItemsToDrop.Add(70, r.Next(1, 3));
@@ -112,12 +115,12 @@ namespace SlimeProduce
                             }
                             goto DropItems;
                         }
-                        else if (Config.EnableSpecialWhiteSlimeDrops && white.Contains(StrToColor(p.Value.orderData)))
+                        else if (Config.EnableSpecialWhiteSlimeDrops && white.Contains(StrToColor(p.Value.orderData.Value)))
                         {
-                            if (StrToColor(p.Value.orderData).R % 2 == 1)
+                            if (StrToColor(p.Value.orderData.Value).R % 2 == 1)
                             {
                                 ItemsToDrop.Add(338, r.Next(2, 5));
-                                if (StrToColor(p.Value.orderData).G % 2 == 1)
+                                if (StrToColor(p.Value.orderData.Value).G % 2 == 1)
                                 {
                                     ItemsToDrop[338] += r.Next(2, 5);
                                 }
@@ -126,13 +129,13 @@ namespace SlimeProduce
                             {
                                 ItemsToDrop.Add(380, r.Next(10, 21));
                             }
-                            if (StrToColor(p.Value.orderData).R % 2 == 0 && StrToColor(p.Value.orderData).G % 2 == 0 && StrToColor(p.Value.orderData).B % 2 == 0 || StrToColor(p.Value.orderData).Equals(Color.White))
+                            if (StrToColor(p.Value.orderData.Value).R % 2 == 0 && StrToColor(p.Value.orderData.Value).G % 2 == 0 && StrToColor(p.Value.orderData.Value).B % 2 == 0 || StrToColor(p.Value.orderData.Value).Equals(Color.White))
                             {
                                 ItemsToDrop.Add(72, r.Next(1, 3));
                             }
                             goto DropItems;
                         }
-                        else if (Config.EnableSpecialPurpleSlimeDrops && purple.Contains(StrToColor(p.Value.orderData)) && int.Parse(p.Value.orderData.Value.Split('.')[4]) % (Convert.ToBoolean(p.Value.orderData.Value.Split('.')[5]) ? 4 : 2) == 0)
+                        else if (Config.EnableSpecialPurpleSlimeDrops && purple.Contains(StrToColor(p.Value.orderData.Value)) && int.Parse(p.Value.orderData.Value.Split('.')[4]) % (Convert.ToBoolean(p.Value.orderData.Value.Split('.')[5]) ? 4 : 2) == 0)
                         {
                             ItemsToDrop.Add(386, r.Next(5, 11));
                             if (Convert.ToBoolean(p.Value.orderData.Value.Split('.')[5]) && Game1.random.NextDouble() < 0.072)
@@ -142,7 +145,7 @@ namespace SlimeProduce
                             goto DropItems;
                         }
                         foreach (DropTable p2 in Config.DropTables){
-                            if (p2.colorRange.Contains(StrToColor(p.Value.orderData)))
+                            if (p2.colorRange.Contains(StrToColor(p.Value.orderData.Value)))
                             {
                                 foreach (ItemDrop d in p2.itemDrops)
                                 {
@@ -156,7 +159,7 @@ namespace SlimeProduce
                         }
 
                         DropItems:
-                        if (grabber != null && e.Location.farmers.Count == 0 && (DeluxeGrabberLoaded || DeluxeGrabberReduxLoaded))
+                        if (grabber != null && e.Location.farmers.Count == 0 && DeluxeGrabberReduxLoaded)
                         {
                             bool full = (grabber.heldObject.Value as Chest).items.CountIgnoreNull() >= Chest.capacity;
                             foreach (KeyValuePair<int, int> p2 in ItemsToDrop)
@@ -171,7 +174,7 @@ namespace SlimeProduce
                         {
                             foreach (KeyValuePair<int, int> p3 in ItemsToDrop)
                             {
-                                Game1.createMultipleObjectDebris(p3.Key, (int)p.Value.TileLocation.X, (int)p.Value.TileLocation.Y, p3.Value, 1f + ((Game1.player.FacingDirection == 2) ? 0f : ((float)Game1.random.NextDouble())));
+                                Game1.createMultipleObjectDebris(p3.Key, (int)p.Value.TileLocation.X, (int)p.Value.TileLocation.Y, p3.Value, Game1.player.UniqueMultiplayerID, e.Location);
                             }
                         }
                     }
@@ -193,19 +196,26 @@ namespace SlimeProduce
         {
             var editor = asset.AsImage();
 
-            Texture2D sourceImage = help.Content.Load<Texture2D>(PathUtilities.NormalizeAssetName("assets/SlimeBallGray.png"), ContentSource.ModFolder);
+            Texture2D sourceImage = ModHelper.Content.Load<Texture2D>(PathUtilities.NormalizeAssetName("assets/SlimeBallGray.png"), ContentSource.ModFolder);
             editor.PatchImage(sourceImage, targetArea: new Rectangle(0, 224, 96, 32));
         }
 
         private void SpawnSlime(string command, string[] args)
         {
-            int red = int.Parse(args[0]);
-            int green = int.Parse(args[1]);
-            int blue = int.Parse(args[2]);
-            Color color = new Color(red, green, blue);
-            GreenSlime slime = new GreenSlime(Game1.player.lastPosition, color);
+            int red, green, blue;
+
+            try {
+                red = int.Parse(args[0]);
+                green = int.Parse(args[1]);
+                blue = int.Parse(args[2]);
+            }
+            catch {
+                Monitor.Log("Could not parse arguments, please ensure they are formatted correctly.");
+                return;
+            }
+
             Monitor.Log($"Spawning slime with color {red}, {green}, {blue}.", LogLevel.Debug);
-            Game1.currentLocation.characters.Add(slime);
+            Game1.currentLocation.characters.Add(new GreenSlime(Game1.player.lastPosition, new Color(red, green, blue)));
         }
 
         public static string ColorFormat(string c)
@@ -229,11 +239,10 @@ namespace SlimeProduce
             return c;
         }
 
-        public static IModHelper help;
+        internal static IModHelper ModHelper;
         internal ModConfig Config;
         readonly ColorRange purple = new ColorRange(new int[] { 151, 255 }, new int[] { 0, 49 }, new int[] { 181, 255 });
         readonly ColorRange white = new ColorRange(new int[] { 231, 255 }, new int[] { 231, 255 }, new int[] { 231, 255 });
-        private static bool DeluxeGrabberLoaded;
         private static bool DeluxeGrabberReduxLoaded;
         private Dictionary<Color, List<int>> ColoredObjects = new Dictionary<Color, List<int>>();
 
